9bfa7383f7367f34be1eb1c06ea17a3a8c2d16b2
Ngum12 <d.ngum@alustudent.com>
2024-09-14 08:38:39 -0700

    Size me please

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+ng the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """Calculating the shape of a matrix"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape

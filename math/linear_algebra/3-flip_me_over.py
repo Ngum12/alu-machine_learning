4cb456f2443af5d87d3709cb2c3867faacbcf00b
Ngum12 <d.ngum@alustudent.com>
2024-09-14 08:39:35 -0700

    Flip me Over

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,19 @@
+ng the transpose of a matrix"""
+
+
+def matrix_transpose(matrix):
+    """Getting the transpose of a matrix"""
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    transpose = []
+
+    for i in range(columns):
+
+        transposed_row = []
+
+        for j in range(rows):
+
+            transposed_row.append(matrix[j][i])
+        transpose.append(transposed_row)
+    return transpose
